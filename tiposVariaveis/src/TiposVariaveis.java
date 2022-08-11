ac4716080d147165dcbc2ea61aac71162cfb364c
Adriel Carmo <adrieldovale7@gmail.com>
2022-08-11 02:13:29 -0400

    Projeto para exemplo de Tipos e Variáveis.

--- tiposVariaveis/src/TiposVariaveis.java
+++ tiposVariaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        byte idade = 18;
+        short ano = 2022;
+        int cep = 79654232; // se começar com 0, o número será desconsiderado na compilação
+        long cpf = 98567432422L; // se começar com 0, o número será desconsiderado na compilação
+        float PI = 3.1415F;
+        double salario = 1275.33;
+
     }
 }
