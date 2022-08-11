4fcfd0bd648413476857f92c2bc04eb606f10196
Adriel Carmo <adrieldovale7@gmail.com>
2022-08-11 02:28:19 -0400

    Projeto para exemplo de Tipos e Variáveis.

--- tiposVariaveis/src/TiposVariaveis.java
+++ tiposVariaveis/src/TiposVariaveis.java
@@ -6,12 +6,12 @@ public class TiposVariaveis {
         short ano = 2022;
         int cep = 79654232; // se começar com 0, o número será desconsiderado na compilação
         long cpf = 98567432422L; // se começar com 0, o número será desconsiderado na compilação
-        float PI = 3.1415F;
+        float OUTRO_PI = 3.1415F;
         double salario = 1275.33;
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal; // não é possível fazer isso, pois o tipo short não comporta a possível precisão do tipo int
+        // short numeroCurto2 = numeroNormal; // não é possível fazer isso, pois o tipo short não comporta a possível precisão do tipo int
 
         // CONSTANTES
         final double PI = 3.1415;
