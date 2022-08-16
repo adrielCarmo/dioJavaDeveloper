490062915407cc9d1a33a69eca882d757fa6c6fa
Adriel Carmo <adrieldovale7@gmail.com>
2022-08-16 14:40:49 -0400

    Criação da documentação do projeto, pelo JAVADOC.

---
+++ tiposVariaveis/src/javadoc/Calculadora.java
@@ -0,0 +1,24 @@
+package javadoc;
+
+/**
+    * <h1>Calculadora</h1>
+    * A Calculadora realiza operações matemáticas entre números inteiros
+    * <p>
+    * <b>Note:</b> Leia atentamente a documentação desta classes
+    * para desfrutar dos recursos oferecidos pelo autor
+    * @author  Adriel Carmo
+    * @version 1.1
+    * @since   16/08/2022
+*/
+    public class Calculadora {
+    /**
+        * Este método é utilizado para somar dois números inteiros
+        * @param numeroUm este é o primeiro parâmetro do método
+        * @param numeroDois este é o segundo parâmetro do método
+        * @return int o resultado deste método é a soma dos dois números.
+    */
+    
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
+}
