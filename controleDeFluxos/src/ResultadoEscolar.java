08d8058807dc3934fe4fceb14a91e127575ffd05
Adriel Carmo <adrieldovale7@gmail.com>
2022-08-19 01:34:21 -0400

    Exemplo de Estruturas Condicionais Compostas.

---
+++ controleDeFluxos/src/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        
+        // exemplo para condicional COMPOSTA:
+        int nota = 6;
+       
+        // se a nota do aluno for maior que a media, o programa mostra aprovado, senão, mostra reprovado
+        if(nota >= 7) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
